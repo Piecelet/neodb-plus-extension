024e2a0866d74d296f999df785638ebadeff9725
lcandy2 <45784494+lcandy2@users.noreply.github.com>
2025-09-30 16:44:24 +0800

    cleanup

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -1,9 +1,5 @@
-import { addFromNeoDB } from "./add-from-neodb";
 import { registerDoubanRatingHandler } from "./douban-rating";
-import { registerNetworkHeaderSpoofing } from "./network-headers";
 
 export default defineBackground(() => {
-  addFromNeoDB();
-  registerNetworkHeaderSpoofing();
   registerDoubanRatingHandler();
 });
